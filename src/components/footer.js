b8e9035f95a49cee97e520e2ab444fa6aca283ae
Muhammad Umair Khan <M.Umair4290@gmail.com>
2025-04-26 00:43:10 +0500

    Simple Navbar and Footer Added

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Footer() {
+    return (
+        <>
+            <footer className='bg-dark text-light'>
+                <p className='text-center'>&copy; 2025, All Rights Reserved.</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FaUserCircle } from "react-icons/fa";
 
-function navbar() {
+function Navbar() {
     return (
         <>
-
+            <nav className='bg-dark text-light py-2'>
+                <div className='container d-flex justify-content-between align-items-center'>
+                    <div className='fs-4'><Link to="/" className='text-decoration-none text-light'>RENTEFI</Link></div>
+                    <div><Link to="/profile" className='text-decoration-none text-light'><FaUserCircle size={28} /></Link></div>
+                </div>
+            </nav>
         </>
     )
 }
 
-export default navbar
+export default Navbar
