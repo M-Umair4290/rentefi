bc56188b5119676ea5093e44e3ffb8e01124bf99
Muhammad Umair Khan <M.Umair4290@gmail.com>
2025-04-27 15:04:51 +0500

    Login page updated, files extensions and names changed

---
+++ src/pages/Login.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import styles from './login.module.css'
+// import MyImage from '../assets/loginphoto.jpg'
+
+function Login() {
+
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    // Current Credentials (Without Backend)
+    const validEmail = 'admin@rentefi.com';
+    const validPassword = '12345';
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        if (email === validEmail && password === validPassword) {
+            setError('');
+            // Redirect to dashboard or homepage after successful login
+            navigate('/dashboard');
+        } else {
+            setError('Invalid email or password.');
+        }
+    };
+
+    return (
+        <>
+            <div className='container-fluid'>
+                <div className='row vh-100'>
+                    <div className={`col-6  ${styles.logincover}`}>
+                    </div>
+
+
+                    <div className='col-6 d-flex align-items-center justify-content-evenly'>
+                        <form className='d-flex flex-column justify-content-center' onChange={handleSubmit}>
+                            <h1>Login</h1>
+                            <p>Access to the Rentefi system</p>
+
+                            <input className='mt-4 mb-2' type='email' name='email' size={40} placeholder='Email' value={email} required onChange={(event) => setEmail(event.target.value)}></input>
+
+                            <input className='my-2' type='password' name='password' size={40} placeholder='Password' value={password} required onChange={(event) => setPassword(event.target.value)}></input>
+
+                            {error && <p style={{ color: 'red', marginTop: '10px' }}>{error}</p>}
+
+                            <button type='submit' className='my-5'>Login</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login
